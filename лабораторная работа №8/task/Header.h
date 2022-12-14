11e52954bc8079525b3672e9ed1e8f173be184d1
Приловский Виктор <112697876+vainarito@users.noreply.github.com>
2022-12-14 15:54:55 +0300

    Add files via upload

---
+++ лабораторная работа №8/task/Header.h
@@ -0,0 +1,9 @@
+#pragma once       
+#include <stdio.h>             
+#include <stdlib.h>			   
+#include <Windows.h>           
+
+void sortStrings(char**, int, int);
+int str_len(char*);
+char** inputStrings(int, char**);
+void printStrings(char**, char*, int);
