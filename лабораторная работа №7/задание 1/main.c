ef69cd1d8f3a625b7c52a97ceb20ebb00643350d
Приловский Виктор <112697876+vainarito@users.noreply.github.com>
2022-12-14 15:49:42 +0300

    Create main.c

---
+++ лабораторная работа №7/задание 1/main.c
@@ -0,0 +1,12 @@
+#include "Header.h"        
+#include <stdio.h>             
+#include <stdlib.h>			   
+#include <Windows.h>           
+
+int main()
+{
+	SetConsoleCP(1251); 
+	mainMenu();
+	return 1;
+}
+
