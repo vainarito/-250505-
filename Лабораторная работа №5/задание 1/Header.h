4cd5db15f0563653a6d6c3128227a370047144c3
Приловский Виктор <112697876+vainarito@users.noreply.github.com>
2022-12-14 15:33:08 +0300

    Add files via upload

---
+++ Лабораторная работа №5/задание 1/Header.h
@@ -0,0 +1,13 @@
+int sizeArr();
+int* mamoryArr(int* array, int size);
+void enterArr1(int* array, int size);
+void enterArr2(int* array, int size);
+int inputSelection();
+void switchArr(int* array, int size);
+void outputArr(int* array, int size);
+int K_Arr();
+void deliteArr(int* array, int size, int k);
+int newSize(int* array, int size, int k);
+int* newArr(int* array, int size);
+void freeArr(int* array);
+
